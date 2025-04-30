9a57ca7265434cae3ef4e840902482c5bcb0f86a
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2025-04-30 16:57:01 +0330

    Remove lint GitHub Action and update pnpm-lock.yaml
    The lint GitHub workflow file has been deleted and updates have been made to the pnpm-lock.yaml file, including changes to dependencies, devDependencies, and lockfile format from version 6.0 to version 9.0. These updates improve compatibility and ensure dependencies align with the latest versions.

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-primary">
+    click me <Icon name="tabler:map" />
+  </button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   modules: ["@nuxt/eslint"],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
