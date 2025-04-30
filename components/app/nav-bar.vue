7d5bdb459dd0fdc54b4772a859ec7b8653e0098a
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2025-04-30 22:21:33 +0330

    feat: add landing page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink tp="/" class="btn btn-ghost text-xl">
+        Travel log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In With Github <icon name="tabler:brand-github" size="24" />
+      </button>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content min-h-96 text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+          quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github <icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
